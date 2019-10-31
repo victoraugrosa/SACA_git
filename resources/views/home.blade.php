fcf3a084ae5b4712a7a719ae8964b40ca4a23f70
victoraugrosa <victoraugustorosa@gmail.com>
2019-10-31 14:41:25 -0300

    Agora ta indo, Front-End teste

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row" style="text-align: center; margin-top:10%; margin-left:10px; font-size:18px;">
+  <div class="col-1 bg-white" style="border-color: black; border-style:solid; border-radius:20px; border-color:gray;">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <a class="nav-link btn btn-outline-dark" id="v-pills-home-tab" data-toggle="button" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+    </div>
+  </div>
+</div>
+<!--
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><b>Usuário</b></div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Teste
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+@endsection
